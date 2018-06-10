a0ba88f4c1e3823af120e7f64f861b3cbd53a640
Gabriel Mesquita <gabriel_t_ana@hotmail.com>
2018-06-10 14:54:00 -0300

    Estrutura de Testes
    Criando a estrutura de diretorios das classes de teste

---
+++ War_Game/test/control/InicializadorDeJogoTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author User
+ *
+ */
+public class InicializadorDeJogoTest {
+
+	/**
+	 * Test method for {@link control.InicializadorDeJogo#ordenaJogadoresAleatoriamente(java.util.List)}.
+	 */
+	@Test
+	public void testOrdenaJogadoresAleatoriamente() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link control.InicializadorDeJogo#inicializaDivisaoTerritorio(java.util.List, java.util.List)}.
+	 */
+	@Test
+	public void testInicializaDivisaoTerritorio() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link control.InicializadorDeJogo#inicializaObjetivo()}.
+	 */
+	@Test
+	public void testInicializaObjetivo() {
+		fail("Not yet implemented");
+	}
+
+}
