ebe3ee3452c589b184e34618c262a35b4d7de980
Gabriel Mesquita <gabriel_t_ana@hotmail.com>
2018-06-10 15:41:29 -0300

    Primeiro Teste
    Implementado o primeiro teste testOrdenaJogadoresAleatoriamente()

--- War_Game/test/control/InicializadorDeJogoTest.java
+++ War_Game/test/control/InicializadorDeJogoTest.java
@@ -4,8 +4,14 @@
 package control;
 
 import static org.junit.Assert.*;
+import model.master.Jogador;
+import model.master.Objetivo;
 
 import org.junit.Test;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author User
@@ -18,7 +24,33 @@ public class InicializadorDeJogoTest {
 	 */
 	@Test
 	public void testOrdenaJogadoresAleatoriamente() {
-		fail("Not yet implemented");
+		/* 
+		 * Esta função de teste serve para testar se a função retorna uma lista de jogadores
+		 * com todos os jogadores ainda contido nela.
+		 * Obs1: Para verificar se a função nao esta engolindo jogadores ( Passa 4 jogadores e ela retorna jogadores diferentes )
+		 * Obs2: Esta função nao testa se os jogadores foram realmente ordenados aleatoriamente, pois isto é inviavel ja que é 'Aleatorio' o ordenamento
+		 */
+		
+		Objetivo objetivoTeste = new Objetivo(1 , "Objetivo de teste");
+		
+		Jogador guilherme = new Jogador("Guilherme" , Color.WHITE, objetivoTeste);
+		Jogador gabriel = new Jogador("Gabriel" , Color.BLUE, objetivoTeste);
+		Jogador prea = new Jogador("Prea" , Color.BLACK, objetivoTeste);
+		Jogador paje = new Jogador("Paje" , Color.YELLOW, objetivoTeste);
+		
+		List<Jogador> listaJogadores = new ArrayList<>();
+		listaJogadores.add(guilherme);
+		listaJogadores.add(gabriel);
+		listaJogadores.add(prea);
+		listaJogadores.add(paje);
+		
+		List<Jogador> listaOrdenadaAleatoriamente = control.InicializadorDeJogo.ordenaJogadoresAleatoriamente(listaJogadores);
+		
+		assertTrue(listaOrdenadaAleatoriamente.size() == listaJogadores.size()); // Testa se a lista tem o mesmo número de jogadores após passar pela função
+		assertTrue(listaOrdenadaAleatoriamente.contains(guilherme));             // Testa se o jogador GUILHERME ainda esta na lista ordenada aleatoriamente
+		assertTrue(listaOrdenadaAleatoriamente.contains(gabriel));               // Testa se o jogador GABRIEL   ainda esta na lista ordenada aleatoriamente
+		assertTrue(listaOrdenadaAleatoriamente.contains(prea));      			 // Testa se o jogador PREA      ainda esta na lista ordenada aleatoriamente
+		assertTrue(listaOrdenadaAleatoriamente.contains(paje));      			 // Testa se o jogador PAJE      ainda esta na lista ordenada aleatoriamente
 	}
 
 	/**
